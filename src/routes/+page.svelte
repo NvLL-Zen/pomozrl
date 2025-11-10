0d645e26d8038a7d022c96a4c7fdd686135e129b
OjanCB <nxll.zen@gmail.com>
2025-11-10 10:36:14 +0700

    Adjusted progress bar horizontal position

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -106,10 +106,10 @@
     const updateBar = (opt) => {
 		console.log('updating bar')
         if (opt == 'reset') {
-            progressBar.style.transform = `translateY(85vh) translateX(-100vw)`
+            progressBar.style.transform = `translateY(90vh) translateX(-100vw)`
             return;
         } else {
-            progressBar.style.transform = `translateY(85vh) translateX(-${(timer/modeTimer)*100}vw)`
+            progressBar.style.transform = `translateY(90vh) translateX(-${(timer/modeTimer)*100}vw)`
             console.log(timer/modeTimer)
         }
     }
